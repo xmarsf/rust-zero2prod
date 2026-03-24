d2f766d38378b5df7c3b53fa00bd7bc1d49f14e9
TruongPX <truong@vdx.vn>
2026-03-24 17:42:49 +0700

    [IMP] : update

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,10 @@
-
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
 use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port  = listener.local_addr().unwrap().port();
+    let port = listener.local_addr().unwrap().port();
     let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
@@ -16,8 +15,11 @@ async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
-
-    let response = client.get(&format!("{address}/health_check")).send().await.expect("Failed to execute request");
+    let response = client
+        .get(&format!("{address}/health_check"))
+        .send()
+        .await
+        .expect("Failed to execute request");
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
@@ -26,9 +28,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
-    let configuration = get_configuration().expect("Failed to read configuration");
+    let mut configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string).await.expect("Failed to connect to Postgres.");
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -41,6 +46,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
     // Assert
     assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
@@ -50,7 +62,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
     for (invalid_body, error_message) in test_cases {
         // Act
